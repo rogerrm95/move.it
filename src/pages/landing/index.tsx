9ecb326942c12c72db8e321166cdf62347fd7f96
rogerrm95 <rogerrm@live.com>
2021-09-03 21:47:05 -0300

    Add: Landing Page

--- src/pages/landing/index.tsx
+++ src/pages/landing/index.tsx
@@ -1,21 +1,35 @@
+import {useRouter} from 'next/router'
+
+import { FiLogIn } from 'react-icons/fi'
 import styles from './../../styles/pages/LandingPage.module.scss'
 
 export default function LandingPage() {
+    const {push} = useRouter()
+
     return (
-        <main className={styles.container}>
-            <section className={styles.description}>
-                <img src="./logo-full.svg" alt="Logo" />
+        <div className={styles.container}>
+            <main className={styles.main}>
+                <section className={styles.description}>
+                    <img src="./logo-full.svg" alt="Logo" />
 
-                <h1>
-                    O lugar ideal para você, exercitar seu corpo enquanto trabalha.
-                </h1>
+                    <h1>
+                        O lugar ideal para você, <br/>
+                        exercitar seu corpo enquanto trabalha.
+                    </h1>
 
-                <p>
-                    Venha participar de nosso jogo. Basta clicar no botão abaixo para conhecer nossa plataforma.
-                </p>
-            </section>
+                    <p>
+                        Venha participar de nosso jogo.<br/>
+                        Basta clicar no botão abaixo para conhecer nossa plataforma.
+                    </p>
 
-            <img src="./assets/hero-image.svg" alt="Pessoas Exercitando" />
-        </main>
+                    <button className={styles.beginButton} onClick={() => push('/login')}>
+                        Entrar
+                        <FiLogIn size={24} color='#FFF' />
+                    </button>
+                </section>
+
+                <img src="./assets/hero-image.svg" alt="Pessoas Exercitando" />
+            </main>
+        </div>
     )
 }
