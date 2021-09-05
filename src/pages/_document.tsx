7d227d16503092526e209ed3cd596b6fd0e3cd32
rogerrm95 <rogerrm@live.com>
2021-09-04 23:13:09 -0300

    Add: Login page

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -6,9 +6,11 @@ export default class MyDocument extends Document {
             <Html>
                 <Head>
                     <link rel='preconnect' href='https://fonts.gstatic.com' />
-                    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600&family=Rajdhani:wght@600&display=swap"
-                        rel="stylesheet" />
-                    <link rel="shortcut icon" href="favicon.png" type='image/png'/>
+                    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600&family=Rajdhani:wght@600&display=swap" rel="stylesheet" />
+                    <link rel="preconnect" href="https://fonts.googleapis.com" />
+                    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap" rel="stylesheet" />
+
+                    <link rel="shortcut icon" href="favicon.png" type='image/png' />
                 </Head>
 
                 <body>
--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,9 +1,38 @@
+import { FiLogIn } from 'react-icons/fi'
 import styles from './../../styles/pages/Login.module.scss'
 
 export default function LandingPage() {
     return (
         <main className={styles.container}>
-            LOGIN
+            <aside className={styles.asideBox}>
+                <img src="./assets/hero-login-image.svg" alt="Mulher alongando" />
+                <h1>
+                    Do conforto de sua casa <br/>
+                    ou trabalho.
+                </h1>
+                <span>
+                    Pratique inúmeros exercícios <br/>
+                    físicos e mantenha-se saudável.
+                </span>
+            </aside>
+
+            <section className={styles.loginBox}>
+                <img src="./assets/fitness.svg" alt="Boneco malhando" />
+                <h1>
+                    Iniciar Sessão
+                </h1>
+                <div className={styles.buttonGroup}>
+                    <span>Selecione uma conta</span>
+                    <button>
+                        Entrar com Google
+                        <FiLogIn size={24} color='#FFF'/>
+                    </button>
+                    <button>
+                        Entrar com Github
+                        <FiLogIn size={24} color='#FFF'/>
+                    </button>
+                </div>
+            </section>
         </main>
     )
 }
